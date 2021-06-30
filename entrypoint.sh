3485d130aeb9a49f808647ce7fde15d7474999bb
Michael Ploof <michaelp@halasystems.com>
2021-06-30 14:41:25 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This causes the linters to return a non-zero exit code when an issue is found,
+# which is necessary to cause a failed GH action workflow result
+set -eu
+
 echo "Running flake8"
 flake8
 echo "Passed flake8 linting!"
